11b46f2a25fa1e889115347d0dd80e53a67cd4b3
Sara Wendland <info@rosebudsara.com>
2023-07-02 02:00:42 -0500

    initialized file structure, and created header/navbar

---
+++ src/App.js
@@ -0,0 +1,18 @@
+// We import useState and useEffect in our component
+import React, { useState, useEffect } from 'react';
+import NavBar from './components/Nav';
+// import Project from './components/Project';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      
+    </div>
+   
+  )
+}
+
+export default App;
