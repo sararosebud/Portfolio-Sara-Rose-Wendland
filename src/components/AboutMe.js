a89bd853763f0f1c39079f2091777e6c0ccddab4
Sara Wendland <info@rosebudsara.com>
2023-07-10 15:55:33 -0500

    added image to about section

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -4,44 +4,63 @@ import { TypeAnimation } from 'react-type-animation';
 import '../styles/aboutme.css'
 
 
+import Card from 'react-bootstrap/Card';
+
+
+
 function About() {
   return (
-    <Accordion defaultActiveKey={['0']} alwaysOpen>
-    <Accordion.Item eventKey="0">
-      <Accordion.Header>About Me:</Accordion.Header>
-      <Accordion.Body>
-      <div>
-      <TypeAnimation
-        sequence={[
-          "Attempting to Code Hello Worl",
-          1000,
-          "Attempting to Code World Peace",
-          1000,
-          "Attempting to Code World Peace One Line at a Time!",
-          1000,
-        ]}
-        speed={50}
-        repeat={0}
-        style={{ fontSize: '2em', 
-                  color: 'white',
-                fontFamily: 'Caprasimo-Regular' }}
-      />
-    </div>
-      </Accordion.Body>
-    </Accordion.Item>
-    <Accordion.Item eventKey="1">
-      <Accordion.Header>Accordion Item #2</Accordion.Header>
-      <Accordion.Body>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-        minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-        aliquip ex ea commodo consequat. Duis aute irure dolor in
-        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-        culpa qui officia deserunt mollit anim id est laborum.
-      </Accordion.Body>
-    </Accordion.Item>
-  </Accordion>
+
+   
+        <><Card className="text-white heroAbout">
+      
+      <Card.ImgOverlay>
+        <Card.Title style={{
+            fontSize: '2.6em',
+            color: 'white',
+            fontFamily: 'Orbitron',
+            textAlign: 'left',  
+              marginLeft: '20px' , 
+              marginTop: '25%',
+          }}>Hi There! 
+          I'm Sara Rose Wendland!</Card.Title>
+        <Card.Text style={{
+              fontSize: '1em',
+              color: 'white',
+              fontFamily: 'Caprasimo-Regular',
+              textAlign: 'left',  
+              marginTop: '10px',
+              marginLeft: '20px'  
+            }}>
+
+        
+        <TypeAnimation
+          sequence={[
+            "Attempting to Code Hello Worl",
+            1000,
+            "Attempting to Code World Peace",
+            1000,
+            "Attempting to Code World Peace One Line at a Time!",
+            1000,
+          ]}
+          speed={50}
+          repeat={0}
+          style={{
+            fontSize: '2em',
+            color: 'white',
+            fontFamily: 'Caprasimo-Regular'
+          }} />
+     
+          
+        </Card.Text>
+        <Card.Text></Card.Text>
+      </Card.ImgOverlay>
+    </Card>
+    
+</>
+      
+
+  
 );
 }
 
